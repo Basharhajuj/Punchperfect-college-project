74079a88be9284916dad09f3829ab0943ab185bc
Basharhajuj <arcademf12@gmail.com>
2025-01-03 02:10:01 +0200

    carousel updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit,ViewChild,ElementRef, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
-import { AuthService } from '../services/auth.service'; // Import AuthService
+import { AuthService } from '../services/auth.service';
+import Swiper from 'swiper';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
     selector: 'app-home',
@@ -9,13 +11,14 @@ import { AuthService } from '../services/auth.service'; // Import AuthService
     styleUrls: ['./home.component.css'],
     standalone: false
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements  AfterViewInit {
   imageList: HTMLElement | null = null;
   slideButtons: NodeListOf<HTMLElement> | null = null;
   sliderScrollbar: HTMLElement | null = null;
   maxScrollLeft = 0;
-  currentSlideIndex = 0; // Track current slide index
-  showPopup = false; // Ensure showPopup is initialized
+  currentSlideIndex = 0;
+  showPopup = false;
+  @ViewChild('swiperRef') swiperRef!: ElementRef;
 
   constructor(
     private router: Router, 
@@ -23,68 +26,49 @@ export class HomeComponent implements OnInit, AfterViewInit {
     private authService: AuthService // Inject AuthService
   ) {}
 
-  ngOnInit(): void {
-    // Initialization logic here if needed
-  }
+  public teamConfig: SwiperOptions = {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 20,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 50,
+      depth: 100,
+      modifier: 0.6,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3, spaceBetween: 30 },
+      768: { slidesPerView: 2, spaceBetween: 20 },
+      0: { slidesPerView: 1, spaceBetween: 10 },
+    },
+  };
+  teamMembers = [
+    { name: 'BOXING GLOVES', role: '', imageUrl: 'https://cdn.shopify.com/s/files/1/0809/0206/2384/files/P00001663.jpg?v=1715110131&width=500', detailsVisible: false },
+    { name: 'BOXING SHOES T', role: '', imageUrl: 'https://cdn.shopify.com/s/files/1/0809/0206/2384/files/eliteshoes_blackgold_1_1_1.jpg?v=1702973090&width=500', detailsVisible: false },
+    { name: 'BOXING SHOES S', role: '', imageUrl: 'https://cdn.shopify.com/s/files/1/0809/0206/2384/files/elite_black_1__2.jpg?v=1702970565&width=500', detailsVisible: false },
+    { name: 'BOXING SHOES M', role: '', imageUrl: 'https://cdn.shopify.com/s/files/1/0809/0206/2384/files/pivt_black_1.jpg?v=1702970662&width=500', detailsVisible: false },
+    { name: 'HEAD GEAR', role: '', imageUrl: 'https://cdn.shopify.com/s/files/1/0809/0206/2384/files/p00001211_1.jpg?v=1702970819&width=500', detailsVisible: false },
+  ];
 
   ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.imageList = document.querySelector(".slider-wrapper .image-list") as HTMLElement;
-      this.slideButtons = document.querySelectorAll(".slider-wrapper .slide-button") as NodeListOf<HTMLElement>;
-      this.sliderScrollbar = document.querySelector(".container .slider-scrollbar") as HTMLElement;
-
-      this.initSlider();
-
-      // Delay showing the popup after 3 seconds (3000 milliseconds) if user is not logged in
-      if (!this.authService.isLoggedIn()) {
-        setTimeout(() => {
-          this.showPopup = true;
-        }, 3000);
-      }
-    } 
-  }
-
-  initSlider(): void {
-    if (this.slideButtons) {
-      this.slideButtons.forEach(button => {
-        button.addEventListener("click", () => {
-          const direction = button.id === "prev-slide" ? -1 : 1;
-          this.currentSlideIndex += direction;
-          this.showSlide(this.currentSlideIndex);
-        });
-      });
+    if (this.swiperRef) {
+      new Swiper(this.swiperRef.nativeElement, this.teamConfig);
     }
 
-    const handleSlideButtons = () => {
-      if (this.slideButtons && this.imageList) {
-        this.slideButtons[0].style.display = this.currentSlideIndex <= 0 ? "none" : "flex";
-        this.slideButtons[1].style.display = this.currentSlideIndex >= this.maxScrollLeft ? "none" : "flex";
-      }
-    };
-
-    if (this.imageList) {
-      this.imageList.addEventListener("scroll", () => {
-        handleSlideButtons();
-      });
-    }
-
-    window.addEventListener("load", () => {
-      if (this.imageList) {
-        this.maxScrollLeft = this.imageList.scrollWidth - this.imageList.clientWidth;
-        handleSlideButtons();
-      }
-    });
   }
 
-  showSlide(index: number): void {
-    const scrollAmount = this.imageList!.clientWidth * index;
-    if (this.imageList) {
-      this.imageList.scroll({
-        left: scrollAmount,
-        behavior: "smooth"
-      });
-    }
-  }
+ 
 
   goToSignInPage(): void {
     this.router.navigate(['/register']);
@@ -92,5 +76,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   closePopup(): void {
     this.showPopup = false;
+  }
+
+  toggleBlur(member: any) {
+    member.detailsVisible = !member.detailsVisible;
   }
 }
